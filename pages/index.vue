c62b81b1114c268436fd0c2d5a7b2dde8e1cbe8a
Patryk Wiśniewski <wisniewski.patryk1997@gmail.com>
2024-09-12 00:24:27 +0200

    first attempt to fetch data

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,28 @@
+<script lang="ts" setup>
+
+const data = ref(null)
+const message = ref("")
+
+onMounted(async () => {
+  try {
+    data.value = await $fetch('http://130.61.148.159:80/post')
+  } catch (e) {
+    console.error('Fetch error:', e)
+  }
+  console.log(data.value)
+})
+
+
+
+
+</script>
+
 <template>
-    <div class="text-red-500">
-        siemanko
-    </div>
+  <div class="bg-slate-300 min-h-screen flex justify-center">
+    <form class="mt-24 flex flex-col">
+      <p>Napisz posta!</p>
+      <textarea class="min-h-24" v-model="message"></textarea>
+      <button>Wyślij</button>
+    </form>
+  </div>
 </template>
